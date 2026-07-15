a052be21c44fd6a4ab62763f43f320281d840c28
Barahmon <sarahtwlin@gmail.com>
2026-07-15 23:14:41 +0800

    build(database): Add package to database, update gitignore for .env file
    test(database): Merged conflicts, add test for database healthcheck API

--- backend-service-nodejs/tests/health.test.js
+++ backend-service-nodejs/tests/health.test.js
@@ -3,9 +3,20 @@ const app = require("../app");
 
 describe("Healthcheck endpoint", () => {
     it("should return 200 OK", async () => {
-        const response =  await request(app).get("/healthcheck/v1");
+        const response =  await request(app).get("/healthcheck/v1/api");
     
         expect(response.statusCode).toBe(200);
         expect(response.body.status).toBe("OK");
+    });
+});
+
+describe("Healthcheck endpoint -- database connection", () => {
+    it("should return 200 OK", async() => {
+        const response = await request(app).get("/healthcheck/v1/db");
+        
+        expect(response.statusCode).toBe(200);
+        expect(response.body.status).toBe("OK");
+        expect(response.body.timestamp).toBeDefined();
+        
     });
 });
