22a05816028647e36f92a658f7043e5645becf26
Barahmon <sarahtwlin@gmail.com>
2026-07-20 02:42:22 +0800

    fix(tasks): Repair prisma connection string, run dev config for prisma migrate

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -1,3 +1,3 @@
-docker compose down
+docker compose --env-file backend-service-nodejs/.env down
 docker compose --env-file backend-service-nodejs/.env up -d --build
 ./scripts/migrate-and-deploy-schema.sh
--- scripts/migrate-and-deploy-schema.sh
+++ scripts/migrate-and-deploy-schema.sh
@@ -1,2 +1,2 @@
-cd ../backend-service-nodejs && npx prisma migrate dev 
+cd backend-service-nodejs && npx dotenv -e .env.dev -- npx prisma migrate dev 
 docker compose exec backend npx prisma migrate deploy
